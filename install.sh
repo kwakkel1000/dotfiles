849be7326083151874fbd1a885efc350bdf4f881
Gijs Kwakkel <gijs.kwakkel@fortanix.com>
2025-07-04 13:10:23 +0200

    more wayland fixes

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-declare -a ConfigItems=("autorandr" "compton.conf" "i3" "nvim" "polybar" "rofi")
+declare -a ConfigItems=("autorandr" "compton.conf" "i3" "sway" "waybar" "shikane" "nvim" "polybar" "rofi")
 
 # Read the array values with space
 for config_item in "${ConfigItems[@]}"; do
     ln -s ".config/$config_item" "$HOME/.config/$config_item"
 done
+
+cp .config/sway/sway-session.target $HOME/.config/systemd/user/sway-session.target
+# cp .config/shikane/shikane.service $HOME/.config/systemd/user/shikane.service
 
 echo "https://github.com/lsd-rs/lsd/releases"
