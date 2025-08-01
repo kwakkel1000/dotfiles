7d0387a4b2c2e7ce2528b9b6d0bf182e274fe800
Gijs Kwakkel <gijs.kwakkel@fortanix.com>
2025-08-01 10:11:14 +0200

    changes

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ for config_item in "${ConfigItems[@]}"; do
     ln -s ".config/$config_item" "$HOME/.config/$config_item"
 done
 
+sudo apt install sway-notification-center
+
 cp .config/sway/sway-session.target $HOME/.config/systemd/user/sway-session.target
 # cp .config/shikane/shikane.service $HOME/.config/systemd/user/shikane.service
 
 echo "https://github.com/lsd-rs/lsd/releases"
+
+mkdir "$HOME/bin"
+ln -s "bin/readcert" "$HOME/bin/readcert"
