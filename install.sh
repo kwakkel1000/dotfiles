3bce809d5198e99bc796d32f5dd598cc7cb8cff0
Gijs Kwakkel <gijs.kwakkel@fortanix.com>
2025-12-12 14:57:59 +0100

    add code-mux

--- install.sh
+++ install.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Parse optional flag
+DO_LINKS=true
+while getopts ":l" opt; do
+    case $opt in
+    l) DO_LINKS=true ;; # Always true unless you add a no-links flag
+    \?)
+        echo "Usage: $0 [-l] ( -l runs only links)"
+        exit 1
+        ;;
+    esac
+done
 
 declare -a ConfigItems=("autorandr" "compton.conf" "i3" "sway" "waybar" "shikane" "nvim" "polybar" "rofi" "alacritty")
 
 # Read the array values with space
 for config_item in "${ConfigItems[@]}"; do
-    ln -s "$HOME/dotfiles/.config/$config_item" "$HOME/.config/$config_item"
+    ln -sf "$SCRIPT_DIR/.config/$config_item" "$HOME/.config/$config_item"
 done
 
-ln -s "$HOME/dotfiles/.config/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$SCRIPT_DIR/.config/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$SCRIPT_DIR/bin/readcert" "$HOME/.local/bin/readcert"
+ln -sf "$SCRIPT_DIR/bin/code-mux" "$HOME/.local/bin/code-mux"
 
-sudo apt install sway-notification-center tmux
-
-cp .config/sway/sway-session.target $HOME/.config/systemd/user/sway-session.target
-# cp .config/shikane/shikane.service $HOME/.config/systemd/user/shikane.service
-
-echo "https://github.com/lsd-rs/lsd/releases"
-
-ln -s "bin/readcert" "$HOME/.local/bin/readcert"
+if [ "$DO_LINKS" = false ]; then
+    sudo apt install sway-notification-center tmux
+    cp "$SCRIPT_DIR/.config/sway/sway-session.target" "$HOME/.config/systemd/user/sway-session.target"
+    echo "https://github.com/lsd-rs/lsd/releases"
+fi
